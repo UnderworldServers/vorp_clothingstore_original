8322c738d69840c37aecad3c57eb63375893f12e
Bobertsawesome <43273928+Bobertsawesome@users.noreply.github.com>
2022-07-20 23:52:54 -0500

    Removed unused language, add hints

--- languages/en.lua
+++ languages/en.lua
@@ -1,86 +1,36 @@
 --------------------------------------------------------------------------------------
 ---------------------------------- ENGLISH -------------------------------------------
+--------------------------------------------------------------------------------------
+-------------- DO NOT change any values that are not enclosed in commas --------------
+--------------------------------------------------------------------------------------
 
 Locales["En"] = {
-    PressToOpen = "Press [~e~Space~q~] to access the store", -- Menu title
-    PressGuide = "Rotate <-[A] [D]-> | Cameras [W] or [S]",
+    PressToOpen = "Press [~e~Space~q~] to access the store", 	-- Prompt when in range of tailor
+    PressGuide = "Rotate <-[A] [D]-> | Cameras [W] or [S]",		-- Prompt inside of clothing store
+	CostOverlay = "Total: $",
+	LoadingOverlay = "Loading ...",
     TitleMenuOutfitsUseBtn = "Wear Outfit",
     TitleMenuOutfitsDeleteBtn = "Delete Outfit",
 
-    TitleMenuClothes = "Dressing room", -- Menu title
-    SubTitleMenuClothes = "Dress up your character", -- Menu description
+    TitleMenuClothes = "Purchase Clothing",
+    SubTitleMenuClothes = "Dress up your character",
 
-    NoExistValue = "Does not exist", -- The option it gives when there is no option
+    NoExistValue = "Does not exist", -- Unused
 
     NoMoney = "You do not have enough money",
-    SuccessfulBuy = "You have been charged", -- Shows how much money you have paid
+    SuccessfulBuy = "You have been charged",
 
     TitleMenuOutfits = "Outfits",
-    SubTitleMenuOutfits = "Wear again your saved clothes",
+    SubTitleMenuOutfits = "Select from your saved outfits",
 
-    PlaceHolderNewOutfit = "If you want to save the outfit, name it and save it",
-    ButtonNewOutfit = "Save Outfit",
+    PlaceHolderNewOutfit = "Enter Name to Save",
+    ButtonNewOutfit = "Save",
 	
+    Finish = "Complete Purchase",
 	SelectOutfit = "Confirm Outfit Selection",
+	FinishDesc = "Make sure you have chosen everything correctly.",
 
-
-    MaskValue = "Mask #",
-    HatsValue = "Hat #", -- The options of the hats section
-    GlassesValue = "Glasses #", -- The options of the glasses section
-    NeckwearValue = "Neckwear #", -- The options of the neckwear section
-    TiesValue = "Tie #", -- The options of the ties section
-    ShirtsValue = "Shirt #", -- The options of the shirts section
-    SuspendersValue = "Suspenders #", -- The options of the suspenders section
-    VestsValue = "Vest #", -- The options of the vests section
-    CoatsValue = "Coat #", -- The options of the coats section
-    PonchosValue = "Poncho #", -- The options of the ponchos section
-    CloaksValue = "Cloak #", -- The options of the cloaks section
-    GlovesValue = "Gloves #", -- The options of the gloves section
-    RingsValue = "Ring #", -- The options of the rings section
-    BraceletsValue = "Bracelet #", -- The options of the bracelets section
-    HolstersValue = "Holster #", -- The options of the holsters section
-    BeltsValue = "Belt #", -- The options of the belts section
-    BucklesValue = "Buckle #", -- The options of the buckles section
-    PantsValue = "Pants #", -- The options of the pants section
-    SkirtsValue = "Skirt #", -- The options of the skirts section
-    ChapsValue = "Chaps #", -- The options of the chaps section
-    BootsValue = "Boots #", -- The options of the boots section
-    SpursValue = "Spurs #", -- The options of the spurs section
-    SpatsValue = "Spats #", -- The options of the spats section
-    GauntletsValue = "Gauntlets #", -- The options of the gauntlets section
-    LoadoutsValue = "Loadouts #", -- The options of the gauntlets section
-    AccessoriesValue = "Accessories #", -- The options of the gauntlets section
-    SatchelsValue = "Satchels #", -- The options of the gauntlets section
-    GunbeltAccsValue = "Gunbelt Accs #",
-    NoHatsValue = "Without a Hat", -- The option it gives when you do not wear a hat
-    NoGlassesValue = "Without Glasses", -- The option it gives when you do not wear glasses
-    NoNeckwearValue = "Without Neckwear", -- The option it gives when you do not wear neckwear
-    NoTiesValue = "Without a Tie", -- The option it gives when you do not wear a tie
-    NoShirtsValue = "Without a Shirt", -- The option it gives when you do not wear a shirt
-    NoSuspendersValue = "Without Suspenders", -- The option it gives when you do not wear suspenders
-    NoVestsValue = "Without a Vest", -- The option it gives when you do not wear a vest
-    NoCoatsValue = "Without a Coat", -- The option it gives when you do not wear a coat
-    NoPonchosValue = "Without a Poncho", -- The option it gives when you do not wear a poncho
-    NoCloaksValue = "Without a Cloak", -- The option it gives when you do not wear a cloak
-    NoGlovesValue = "Without Gloves", -- The option it gives when you do not wear gloves
-    NoRingsValue = "Without a Ring", -- The option it gives when you do not wear a ring
-    NoBraceletsValue = "Without a Bracelet", -- The option it gives when you do not wear a bracelet
-    NoHolstersValue = "Without a Holster", -- The option it gives when you do not wear a holster
-    NoBeltsValue = "Without a Belt", -- The option it gives when you do not wear a belt
-    NoBucklesValue = "Without a Buckle", -- The option it gives when you do not wear a buckle
-    NoPantsValue = "Without Pants", -- The option it gives when you do not wear pants
-    NoSkirtsValue = "Without a Skirt", -- The option it gives when you do not wear a skirt
-    NoChapsValue = "Without Chaps", -- The option it gives when you do not wear chaps
-    NoBootsValue = "Without Boots", -- The option it gives when you do not wear boots
-    NoSpursValue = "Without Spurs", -- The option it gives when you do not wear spurs
-    NoSpatsValue = "Without Spats", -- The option it gives when you do not wear spurs
-    NoGauntletsValue = "Without Gauntlets", -- The option it gives when you do not wear spurs
-    NoLoadoutsValue = "Without Loadouts", -- The option it gives when you do not wear spurs
-    NoAccessoriesValue = "Without Accessories", -- The option it gives when you do not wear spurs
-    NoSatchelsValue = "Without Satchels", -- The option it gives when you do not wear spurs
-    NoGunbeltAccsValue = "Without Gunbelt Accs",
-    NoMaskValue = "Without Face Mask", -- The option it gives when you do not wear a face mask
-
+	--------------------------------------- Names Shown In The Purchase Clothing Menu ---------------------------------------
     Hats = "Hats",
     Glasses = "Glasses",
     Masks = "Masks",
@@ -113,38 +63,38 @@ Locales["En"] = {
     Satchels = "Satchels",
     GunbeltAccs = "Gunbelt Accs",
 
-    Finish = "Dress up character", -- Confirms the options of the Menu
-
-    -- Descriptions of the clothing Menu sections
-    HatsDesc = "Choose the hat",
-    GlassesDesc = "Choose the glasses",
-    MasksDesc = "Choose if you want to wear a mask",
-    NeckwearDesc = "Choose the neckwear",
-    TiesDesc = "Choose the tie",
-    ShirtsDesc = "Choose the shirt",
-    SuspendersDesc = "Choose the suspenders",
-    VestsDesc = "Choose the vest",
-    CoatsDesc = "Choose the coat",
-    PonchosDesc = "Choose the poncho",
-    CloaksDesc = "Choose the cloak",
-    GlovesDesc = "Choose the gloves",
-    RightRingsDesc = "Choose the right hand rings",
-    LeftRingsDesc = "Choose the left hand rings",
-    BraceletsDesc = "Choose the bracelet",
-    PrimaryHolstersDesc = "Choose the primary holster",
-    BeltsDesc = "Choose the belt",
-    BucklesDesc = "Choose the buckle",
-    SecondaryHolstersDesc = "Choose the secondary holster",
-    PantsDesc = "Choose the pants",
-    SkirtsDesc = "Choose the skirt",
-    ChapsDesc = "Choose the chaps",
-    BootsDesc = "Choose the boots",
-    SpursDesc = "Choose the spurs",
-    SpatsDesc = "Choose the spats",
-    GauntletsDesc = "Choose the gauntlets",
-    LoadoutsDesc = "Choose the loadouts",
-    AccessoriesDesc = "Choose the accessories",
-    SatchelsDesc = "Choose the satchel",
-    GunbeltAccsDesc = "choose the gunbelt accs",
-    FinishDesc = "Make sure you have chosen everything correctly."
+	--------------------------------------- Descriptions Of The Clothing Menu Sections ---------------------------------------
+    Descriptions = {
+		Hats = "Choose the hat",
+		Glasses = "Choose the glasses",
+		Masks = "Choose if you want to wear a mask",
+		Neckwear = "Choose the neckwear",
+		Ties = "Choose the tie",
+		Shirts = "Choose the shirt",
+		Suspenders = "Choose the suspenders",
+		Vests = "Choose the vest",
+		Coats = "Choose the coat",
+		CoatsClosed = "Choose the coat",
+		Ponchos = "Choose the poncho",
+		Cloaks = "Choose the cloak",
+		Gloves = "Choose the gloves",
+		RightRings = "Choose the right hand rings",
+		LeftRings = "Choose the left hand rings",
+		Bracelets = "Choose the bracelet",
+		PrimaryHolsters = "Choose the primary holster",
+		Belts = "Choose the belt",
+		Buckles = "Choose the buckle",
+		SecondaryHolsters = "Choose the secondary holster",
+		Pants = "Choose the pants",
+		Skirts = "Choose the skirt",
+		Chaps = "Choose the chaps",
+		Boots = "Choose the boots",
+		Spurs = "Choose the spurs",
+		Spats = "Choose the spats",
+		Gauntlets = "Choose the gauntlets",
+		Loadouts = "Choose the loadouts",
+		Accessories = "Choose the accessories",
+		Satchels = "Choose the satchel",
+		GunbeltAccs = "choose the gunbelt accs",
+	}
 }
